d19367b43fc14df9a0b98a38a40a60e1a9cfad83
SATYAM GOYAL <goyalsatyam8@gmail.com>
2020-01-19 22:57:23 +0530

    Headless Browser added with Smart PC Wifi login

--- SRM-WiFi-Auto-Login.py
+++ SRM-WiFi-Auto-Login.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 import time
 
+options = Options()
+options.headless = True
+
 # add the path where you have installed the gecko driver with its name.
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options, executable_path=r'C:\Users\SATYAM\My-GitHub-REPOSITORIES\SRM WIFI LOGIN\geckodriver.exe')
 driver.get('https://192.168.10.3/connect/PortalMain')
 time.sleep(5)
 Username = driver.find_element_by_id("LoginUserPassword_auth_username")
--- run_script.bat
+++ run_script.bat
@@ -1,3 +1,13 @@
+@echo off
+netsh wlan connect ssid="SRM HOSTELS" name="SRM HOSTELS"
+if errorlevel 1 goto END
+
+:ERRORHANDLER
 cd "C:\Users\SATYAM\My-GitHub-REPOSITORIES\SRM WIFI LOGIN"
 
 python SRM-WiFi-Auto-Login.py
+goto END
+
+:END
+pause
+EXIT
