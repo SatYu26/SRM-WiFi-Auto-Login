bbde0d494086ce50efca7380c3efd080dd7d29a2
SATYAM GOYAL <goyalsatyam8@gmail.com>
2020-01-20 16:29:44 +0530

    Batch file modified

---
+++ run_script .bat
@@ -0,0 +1,20 @@
+@echo off
+netsh wlan connect ssid="SRM HOSTELS" name="SRM HOSTELS" >nul 2>&1
+if errorlevel 1 goto END
+
+:ERRORHANDLER
+cd "C:\Users\SATYAM\My-GitHub-REPOSITORIES\SRM WIFI LOGIN"
+
+python SRM-WiFi-Auto-Login.py
+
+goto ENDC
+
+:END
+echo Not able to Connect.
+pause
+EXIT
+
+:ENDC
+echo SUCCESSFULLY CONNECTED TO SRM HOSTEL
+pause
+EXIT
